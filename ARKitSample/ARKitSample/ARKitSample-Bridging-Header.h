09c46f0f25a960d09ebad150ae94b56e7a2ebd75
Marcelo Chaves <marcelochaves17@gmail.com>
2019-04-25 10:08:58 -0300

    Add game scene

---
+++ ARKitSample/ARKitSample/ARKitSample-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  ARKitSample-Bridging-Header.h
+//  ARKitSample
+//
+//  Created by Marcelo Chaves on 08/10/18.
+//  Copyright © 2018 Marcelo Chaves. All rights reserved.
+//
+
+#ifndef ARKitSample-Bridging-Header
+#define ARKitSample-Bridging-Header
+
+#import <CommonCrypto/CommonCrypto.h>
+
+#endif /* ARKitSample-Bridging-Header */
---
+++ ARKitSample/ARKitSample/Types.swift
@@ -0,0 +1,17 @@
+//
+//  Types.swift
+//  ARKitSample
+//
+//  Created by Marcelo Chaves on 25/04/19.
+//  Copyright © 2019 Marcelo Chaves. All rights reserved.
+//
+
+import SpriteKit
+
+enum Sounds {
+  static let fire = SKAction.playSoundFileNamed("sprayBug", waitForCompletion: false)
+  static let hit = SKAction.playSoundFileNamed("hitBug", waitForCompletion: false)
+  static let bugspray = SKAction.playSoundFileNamed("catchBugspray", waitForCompletion: false)
+  static let win = SKAction.playSoundFileNamed("win.wav", waitForCompletion: false)
+  static let lose = SKAction.playSoundFileNamed("lose.wav", waitForCompletion: false)
+}
